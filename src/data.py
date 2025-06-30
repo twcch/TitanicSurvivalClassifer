c3f6835a3075f19e2c4fa8255ff83fdd0ccc5829
Alan Hsieh <twcch1218@gmail.com>
2025-06-30 20:09:36 +0800

    feat: 新增資料讀取功能

---
+++ src/data.py
@@ -0,0 +1,85 @@
+from enum import Enum
+from pathlib import Path
+from typing import Tuple
+
+import pandas as pd
+
+RAW_DATA_PATH = Path("data/raw/")
+PROCESSED_DATA_PATH = Path("data/processed/")
+FEATURES_DATA_PATH = Path("data/features/")
+
+
+class FileName(Enum):
+    TRAIN = "train.csv"
+    TEST = "test.csv"
+    SUBMISSION = "submission.csv"
+
+
+def load_train_raw_data() -> pd.DataFrame:
+    return pd.read_csv(RAW_DATA_PATH / FileName.TRAIN.value)
+
+
+def load_test_raw_data() -> pd.DataFrame:
+    return pd.read_csv(RAW_DATA_PATH / FileName.TEST.value)
+
+
+def load_raw_data() -> Tuple[pd.DataFrame, pd.DataFrame]:
+    train_data = load_train_raw_data()
+    test_data = load_test_raw_data()
+    return train_data, test_data
+
+
+def load_train_processed_data() -> pd.DataFrame:
+    return pd.read_csv(PROCESSED_DATA_PATH / FileName.TRAIN.value)
+
+
+def save_train_processed_data(df: pd.DataFrame) -> None:
+    file_path = PROCESSED_DATA_PATH / FileName.TRAIN.value
+
+    df.to_csv(file_path, index=False)
+
+
+def load_test_processed_data() -> pd.DataFrame:
+    return pd.read_csv(PROCESSED_DATA_PATH / FileName.TEST.value)
+
+
+def save_test_processed_data(df: pd.DataFrame) -> None:
+    file_path = PROCESSED_DATA_PATH / FileName.TEST.value
+
+    df.to_csv(file_path, index=False)
+
+
+def load_processed_data() -> Tuple[pd.DataFrame, pd.DataFrame]:
+    train_data = load_train_processed_data()
+    test_data = load_test_processed_data()
+    return train_data, test_data
+
+
+def load_train_features_data() -> pd.DataFrame:
+    return pd.read_csv(FEATURES_DATA_PATH / FileName.TRAIN.value)
+
+
+def save_train_features_data(df: pd.DataFrame) -> None:
+    file_path = FEATURES_DATA_PATH / FileName.TRAIN.value
+
+    df.to_csv(file_path, index=False)
+
+
+def load_test_features_data() -> pd.DataFrame:
+    return pd.read_csv(FEATURES_DATA_PATH / FileName.TEST.value)
+
+
+def save_test_features_data(df: pd.DataFrame) -> None:
+    file_path = FEATURES_DATA_PATH / FileName.TEST.value
+
+    df.to_csv(file_path, index=False)
+
+
+def load_features_data() -> Tuple[pd.DataFrame, pd.DataFrame]:
+    train_data = load_train_features_data()
+    test_data = load_test_features_data()
+    return train_data, test_data
+
+
+def save_submission(df: pd.DataFrame, submission_path) -> None:
+    df.to_csv(submission_path, index=False)
