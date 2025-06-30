ed510a900376b3e664f6226fb8829d32804f16cf
Alan Hsieh <twcch1218@gmail.com>
2025-06-30 20:15:05 +0800

    feat: 新增模型訓練與預測功能

---
+++ scripts/inference.py
@@ -0,0 +1,65 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+import json
+import argparse
+import numpy as np
+import pandas as pd
+from src.data import load_test_features_data, save_submission
+from src.models.xgboost import XGBoostModel
+
+MODEL_PATH = "models/v1/model_xgb.pkl"
+TEST_FEATURES_PATH = "data/features/test.csv"
+SUBMISSION_PATH = "data/submission/submission.csv"
+
+
+def preprocess_test(test_data, encoder, config):
+    categorical = config["features"]["categorical"]
+    numerical = config["features"]["numerical"]
+
+    if encoder is None:
+        raise ValueError("Encoder not found in model artifacts.")
+
+    X_cat = encoder.transform(test_data[categorical])
+    X_all = np.hstack([X_cat, test_data[numerical].values])
+
+    return X_all
+
+
+def predict_and_save(model, X, passenger_ids, submission_path):
+    predictions = model.predict(X)
+    predictions = predictions.astype(int)
+
+    df = pd.DataFrame({"PassengerId": passenger_ids, "Survived": predictions})
+
+    save_submission(df, submission_path)
+
+
+def main(config_path: str):
+    with open(config_path, "r") as f:
+        config = json.load(f)
+
+    model = XGBoostModel()
+    model.load(config["output"]["model_path"])
+    encoder = model.get_artifact("encoder")
+    test_data = load_test_features_data()
+    X = preprocess_test(test_data, encoder, config)
+    passenger_ids = test_data["PassengerId"]
+
+    submission_path = config["output"]["submission_file"]
+    predict_and_save(model, X, passenger_ids, submission_path)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--config",
+        type=str,
+        default="configs/v1/config.json",
+        help="Path to the configuration file",
+    )
+
+    args = parser.parse_args()
+    main(args.config)
---
+++ scripts/train_model.py
@@ -0,0 +1,83 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+import json
+import argparse
+import numpy as np
+import pandas as pd
+import src.log_writer as lw
+from sklearn.model_selection import train_test_split
+from src.models.xgboost import XGBoostModel
+from src.features.one_hot_feature_encoder import OneHotFeatureEncoder
+
+FEATURES_PATH = "data/features/train.csv"
+MODEL_PATH = "models/v1"
+
+
+def load_data():
+    df = pd.read_csv(FEATURES_PATH)
+
+    return df
+
+
+def preprocess_data(df: pd.DataFrame, config: dict):
+    categorical = config["features"]["categorical"]
+    numerical = config["features"]["numerical"]
+
+    X = df[categorical + numerical]
+    y = df["Survived"]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+
+    # One-hot encoding for categorical features
+    encoder_config = config["feature_engineering"]["encodings"]["onehot"]
+    categorical = encoder_config["features"]
+    encoder_params = encoder_config["params"]
+
+    encoder = OneHotFeatureEncoder(
+        categorical_features=categorical,
+        **encoder_params
+    )
+    X_train_cat = encoder.fit_transform(X_train)
+    X_test_cat = encoder.transform(X_test)
+
+    X_train_all = np.hstack([X_train_cat, X_train[numerical].values])
+    X_test_all = np.hstack([X_test_cat, X_test[numerical].values])
+
+    return X_train_all, X_test_all, y_train, y_test, encoder
+
+
+def main(config_path: str):
+    with open(config_path, "r") as f:
+        config = json.load(f)
+
+    df = load_data()
+    X_train_all, X_test_all, y_train, y_test, encoder = preprocess_data(df, config)
+
+    model = XGBoostModel(**config["model"]["params"])
+    model.fit(X_train_all, y_train)
+    model.set_artifact("encoder", encoder)
+
+    print("Training Accuracy:", model.score(X_train_all, y_train))
+    print("Test Accuracy:", model.score(X_test_all, y_test))
+
+    model.save(path=config["output"]["model_path"])
+
+    lw.save_training_log(config,)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--config",
+        type=str,
+        default="configs/v1/config.json",
+        help="Path to the configuration file",
+    )
+
+    args = parser.parse_args()
+    main(args.config)
